4b7d13f656052d5fbf44f5a23ce55f9e773d355b
AronGomu <neverismine@gmail.com>
2023-12-26 23:09:40 +0100

    git cmd args + random greetings

--- basedGo2/basedGo2.go
+++ basedGo2/basedGo2.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/AronGomu/basedGo/basedGo"
 )
 
 func main() {
 
+	fmt.Println(os.Args)
+
 	log.SetPrefix("basedGo2: ")
 	log.SetFlags(0)
 
-	message, err := basedGo.Hello("")
+	if len(os.Args) < 2 {
+		log.Fatal("basedGo2 has no command line argument !")
+	}
+
+	message, err := basedGo.Hello(os.Args[1])
 
 	if err != nil {
 		log.Fatal(err)
