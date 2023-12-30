655c083f50926bd44027da1f2654827f2be645e3
AronGomu <neverismine@gmail.com>
2023-12-30 10:05:55 +0100

    add tests for basedGo

---
+++ basedGo/basedGo_test.go
@@ -0,0 +1,23 @@
+package basedGo
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Test"
+	// name := 5
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello (%q) = %q, %v, want match for %#q, nil`, name, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello ("") = %q, %v, want "", error`, msg, err)
+	}
+}
